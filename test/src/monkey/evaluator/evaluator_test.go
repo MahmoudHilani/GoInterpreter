63793f8663ff1b0bf71332d3f93b459df0bc75c8
Mahmoud Hilani <mahmoudhilani18@gmail.com>
2024-09-27 07:26:54 +0300

    evaluated nulls and booleans, fixed missing sections

--- test/src/monkey/evaluator/evaluator_test.go
+++ test/src/monkey/evaluator/evaluator_test.go
@@ -7,20 +7,55 @@ import (
 	"testing"
 )
 
-func TestEvalIntegerExpression(t *testing.T) {
+func TestEvalBooleanExpression( t *testing.T) {
 	tests := []struct {
-		input 	string
-		expected int64
+		input    string 
+		expected bool
 	}{
-		{"5", 5},
-		{"10", 10},
+		{"true", true},
+		{"false", false},
 	}
 
 	for _, tt := range tests {
 		evaluated := testEval(tt.input)
-		testIntegerObject(t, evaluated, tt.expected)
+		testBooleanObject(t, evaluated, tt.expected)
 	}
 }
+
+// func TestBangOperator(t *testing.T) {
+// 	tests := []struct {
+// 		input string 
+// 		expected bool
+// 	}{
+// 		{"!true", false},
+// 		{"!false", true},
+// 		{"!5", false},
+// 		{"!!true", true},
+// 		{"!!false", false},
+// 		{"!!5", true},
+// 	}
+
+// 	for _, tt := range tests {
+// 		evaluated := testsEval(tt.input)
+// 		testBooleanObject(t, evaluated, tt.expected)
+// 	}
+// }
+
+
+// func TestEvalIntegerExpression(t *testing.T) {
+// 	tests := []struct {
+// 		input 	string
+// 		expected int64
+// 	}{
+// 		{"5", 5},
+// 		{"10", 10},
+// 	}
+
+// 	for _, tt := range tests {
+// 		evaluated := testEval(tt.input)
+// 		testIntegerObject(t, evaluated, tt.expected)
+// 	}
+// }
 
 func testEval(input string) object.Object {
 	l := lexer.New(input)
@@ -39,6 +74,18 @@ func testIntegerObject(t *testing.T, obj object.Object, expected int64) bool {
 	}
 	if result.Value != expected {
 		t.Errorf("object has wrong value. Got %d, want %d", result.Value, expected)
+		return false 
+	}
+	return true 
+}
+
+func testBooleanObject(t *testing.T, obj object.Object, expected bool) bool {
+	result, ok := obj.(*object.Boolean)
+	if !ok {
+		t.Errorf("Object is not boolean. got %T (%+v)", obj, obj)
+	}
+	if result.Value != expected {
+		t.Errorf("object has wrong value. Got %t want %t", result.Value, expected)
 		return false 
 	}
 	return true 
