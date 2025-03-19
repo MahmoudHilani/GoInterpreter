7c4c648d024d5d3836cec8ac77cefa32dc1d003d
Mahmoud Hilani <mahmoudhilani18@gmail.com>
2025-03-19 17:37:43 +0300

    test: main directory change

--- test/src/monkey/main.go
+++ test/src/monkey/main.go
@@ -1,19 +1,80 @@
+// package main
+
+// import (
+// 	"fmt"
+// 	"os"
+// 	"os/user"
+
+// 	"github.com/MahmoudHilani/GoInterpreter/test/src/monkey/repl"
+// )
+
+// func mmain() {
+// 	user, err := user.Current()
+// 	if err != nil {
+// 		panic(err)
+
+// 	}
+// 	fmt.Printf("Hello %s! Start yapping\n", user.Username)
+// 	repl.Start(os.Stdin, os.Stdout)
+// }
+
 package main
 
 import (
-	"fmt"
-	"os"
-	"os/user"
+	"encoding/json"
+	"log"
+	"net/http"
+    "os"
 
+	"github.com/MahmoudHilani/GoInterpreter/test/src/monkey/object"
 	"github.com/MahmoudHilani/GoInterpreter/test/src/monkey/repl"
 )
 
-func mmain() {
-	user, err := user.Current()
-	if err != nil {
-		panic(err)
+type InterpretRequest struct {
+	Code string `json:"code"`
+}
 
+type InterpretResponse struct {
+	Result string `json:"result,omitempty"`
+	Error  string `json:"error,omitempty"`
+}
+
+func handleInterpret(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.Error(w, "Invalid request method", http.StatusMethodNotAllowed)	
 	}
-	fmt.Printf("Hello %s! Start yapping\n", user.Username)
-	repl.Start(os.Stdin, os.Stdout)
+
+
+
+	var req InterpretRequest
+	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
+		http.Error(w, "Invalid request body", http.StatusBadRequest)
+		return
+	}
+
+	env := object.NewEnvironment()
+	result := repl.StartAPI(req.Code, env)
+
+	resp := InterpretResponse{
+		Result: result,
+	}
+	
+	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+
+	json.NewEncoder(w).Encode(resp)
+}
+
+func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+	http.HandleFunc("/interpret", handleInterpret)
+
+	log.Printf("Starting server on port %s", port)
+	if err := http.ListenAndServe(":"+port, nil); err != nil {
+		log.Fatalf("Failed to start server: %v", err)
+	}
 }
