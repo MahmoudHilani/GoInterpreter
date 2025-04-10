f4210e97562280f998c72347b5976e0f5cd2b84c
Mahmoud Hilani <mahmoudhilani18@gmail.com>
2025-04-10 22:24:51 +0300

    feat: switch to web socket

--- test/src/monkey/main.go
+++ test/src/monkey/main.go
@@ -1,3 +1,4 @@
+// code for local machine
 // package main
 
 // import (
@@ -8,7 +9,7 @@
 // 	"github.com/MahmoudHilani/GoInterpreter/test/src/monkey/repl"
 // )
 
-// func mmain() {
+// func main() {
 // 	user, err := user.Current()
 // 	if err != nil {
 // 		panic(err)
@@ -21,18 +22,12 @@
 package main
 
 import (
-	"encoding/json"
-	"errors"
-	"fmt"
-	"io"
-	"strings"
-
 	"log"
 	"net/http"
 	"os"
 
-	"github.com/MahmoudHilani/GoInterpreter/test/src/monkey/object"
 	"github.com/MahmoudHilani/GoInterpreter/test/src/monkey/repl"
+	"github.com/gorilla/websocket"
 )
 
 type InterpretRequest struct {
@@ -44,99 +39,25 @@ type InterpretResponse struct {
 	Error  string `json:"error,omitempty"`
 }
 
-var env = object.NewEnvironment()
+var upgrader = websocket.Upgrader{}
 
-func handleInterpret(w http.ResponseWriter, r *http.Request) {
-	// Set CORS headers for all responses
-	w.Header().Set("Content-Type", "application/json")
-	w.Header().Set("Access-Control-Allow-Origin", "*")
-	w.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS")
-	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
-
-	ct := r.Header.Get("Content-Type")
-	if ct != "" {
-		mediaType := strings.ToLower(strings.TrimSpace(strings.Split(ct, ";")[0]))
-		if mediaType != "application/json" {
-			msg := "Content-Type header is not application/json"
-			http.Error(w, msg, http.StatusUnsupportedMediaType)
-			return 
-		}
+func handleSocket(w http.ResponseWriter, r *http.Request) {
+	upgrader.CheckOrigin = func(r *http.Request) bool {
+		return true
 	}
-
-	r.Body = http.MaxBytesReader(w, r.Body, 1048576)
-	
-	// Handle preflight OPTIONS request
-	if r.Method == http.MethodOptions {
-		w.WriteHeader(http.StatusOK)
-		return
-	}
-	
-	if r.Method != http.MethodPost {
-		log.Printf("Invalid method: %s", r.Method)
-		http.Error(w, "Invalid request method", http.StatusMethodNotAllowed)
-		return	
-	}
-
-	// Debug request
-	log.Printf("Content-Type: %s", r.Header.Get("Content-Type"))
-	log.Printf("Request length: %d", r.ContentLength)
-	
-	dec := json.NewDecoder(r.Body)
-	dec.DisallowUnknownFields()
-
-	var req InterpretRequest
-	err := dec.Decode(&req)
+	upgrader.ReadBufferSize = 1024
+	upgrader.WriteBufferSize = 1024
+	c, err := upgrader.Upgrade(w, r, nil)
 	if err != nil {
-		var syntaxError *json.SyntaxError
-		var unmarshalError *json.UnmarshalTypeError
-		var maxBytesError *http.MaxBytesError
-
-		switch {
-		case errors.As(err, &syntaxError):
-			msg := fmt.Sprintf("request body contains badly-formed JSON (at position %d)", syntaxError.Offset)
-			http.Error(w, msg, http.StatusBadRequest)
-
-		case errors.Is(err, io.ErrUnexpectedEOF):
-			msg := fmt.Sprint("Request body contains badly-formed JSON")
-			http.Error(w, msg, http.StatusBadRequest)
-
-		case errors.As(err, &unmarshalError):
-			msg := fmt.Sprintf("Request body contains an invalid value for the %q field (at position %d)", unmarshalError.Field, unmarshalError.Offset)
-			http.Error(w, msg, http.StatusBadRequest)
-
-		case strings.HasPrefix(err.Error(), "json: unknown field "):
-			fieldName := strings.TrimPrefix(err.Error(), "json: unknown field ")
-			msg := fmt.Sprintf("Request body contains uknown field: %s", fieldName)
-			http.Error(w, msg, http.StatusBadRequest)
-		
-		case errors.Is(err, io.EOF):
-			msg := "Request body must not be empty"
-			http.Error(w, msg, http.StatusBadRequest)
-
-		case errors.As(err, &maxBytesError):
-			msg := fmt.Sprintf("Request body must not be larger than %d bytes", maxBytesError.Limit)
-			http.Error(w, msg, http.StatusRequestEntityTooLarge)
-
-		default:
-			log.Print(err.Error())
-			http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
-
-		}
+		log.Printf("Websocket upgrade error: %v", err)
 		return
 	}
-	log.Printf("Request: %s", req)
-	log.Printf("Received code: '%s'", req.Code)
-	
-	
-	result := repl.StartAPI(req.Code, env)
-	log.Printf("Interpretation result: %s", result)
-
-	resp := InterpretResponse{
-		Result: result,
-	}
-	
-	json.NewEncoder(w).Encode(resp)
+	defer c.Close()
+	c.SetReadLimit(1048576)
+	repl.StartAPI(c)
 }
+
+
 
 func main() {
 	port := os.Getenv("PORT")
@@ -144,7 +65,7 @@ func main() {
 		port = "8080"
 	}
 
-	http.HandleFunc("/interpret", handleInterpret)
+	http.HandleFunc("/socket", handleSocket)
 
 	log.Printf("Starting server on port %s", port)
 	if err := http.ListenAndServe(":"+port, nil); err != nil {
--- test/src/monkey/repl/repl.go
+++ test/src/monkey/repl/repl.go
@@ -2,17 +2,30 @@ package repl
 
 import (
 	"bufio"
+	"encoding/json"
+	"errors"
 	"fmt"
 	"io"
 	"log"
 	"strings"
+
 	"github.com/MahmoudHilani/GoInterpreter/test/src/monkey/evaluator"
 	"github.com/MahmoudHilani/GoInterpreter/test/src/monkey/lexer"
 	"github.com/MahmoudHilani/GoInterpreter/test/src/monkey/object"
 	"github.com/MahmoudHilani/GoInterpreter/test/src/monkey/parser"
+	"github.com/gorilla/websocket"
 )
 
 const PROMPT = ">> "
+type InterpretRequest struct {
+	Code string `json:"code"`
+}
+
+type InterpretResponse struct {
+	Result string `json:"result,omitempty"`
+	Error  string `json:"error,omitempty"`
+}
+
 
 func Start(in io.Reader, out io.Writer) {
 	scanner := bufio.NewScanner(in)
@@ -40,26 +53,63 @@ func Start(in io.Reader, out io.Writer) {
 			io.WriteString(out, evaluated.Inspect())
 			io.WriteString(out, "\n")
 		}
-
 	}
 }
 
-func StartAPI(in string, env *object.Environment) string {
-		fmt.Print(PROMPT)
-		log.Println(in)
-		l := lexer.New(in)
-		p := parser.New(l)
+func StartAPI(c *websocket.Conn)  {
+	env := object.NewEnvironment()
+	var res string 
+	for {
+		mt, message, err := c.ReadMessage()
+		if err != nil {
+			if websocket.IsUnexpectedCloseError(err, 
+				websocket.CloseGoingAway, 
+				websocket.CloseNormalClosure) {
+				log.Printf("WebSocket read error: %v", err)
+			}
+			break
+		}
+		log.Printf("WebSocket message received: %d, %s", mt, message)
 
+		var req InterpretRequest
+		
+		err = json.Unmarshal(message, &req)
+		if err != nil {
+		errorResponse := createErrorResponse(err)
+		log.Printf("JSON parse error: %v", err)
+		
+		// Send the error response back to the client
+		err = c.WriteJSON(errorResponse)
+		if err != nil {
+			log.Printf("Error sending error response: %v", err)
+			break
+		}
+		continue
+	}
+		l := lexer.New(req.Code)
+		p := parser.New(l)
 		program := p.ParseProgram()
 		if len(p.Errors()) != 0 {
-			return printParserErrorsAPI(p.Errors())
+			 printParserErrorsAPI(p.Errors(), c)
 		}
 
 		evaluated := evaluator.Eval(program, env)
 		if evaluated != nil {
-			return evaluated.Inspect()
+			res = evaluated.Inspect()
 		}
-	return "No result"
+		if evaluated == nil {
+			res = "continue"
+		}
+		resp := InterpretResponse{
+			Result: res,
+		}
+		err = c.WriteJSON(resp)
+		if err != nil {
+			log.Printf("Error writing response: %v", err)
+			break
+		}
+	}
+	
 }
 
 const MONKEY_FACE = `        __,__
@@ -83,10 +133,63 @@ func printParserErrors(out io.Writer, errors []string) {
 	}
 }
 
-func printParserErrorsAPI(errors []string) string {
-
-
-	out := strings.Join(errors, "\n")
-	return out
+func printParserErrorsAPI(errors []string, c *websocket.Conn) {
+	e := strings.Join(errors, "\n")
+	out := MONKEY_FACE + "Woops! We ran into some monkey business here!\n" + e
+	resp := InterpretResponse{
+		Result: out,
+	}
+	err := c.WriteJSON(resp)
+	if err != nil {
+		log.Printf("Error writing response: %v", err)
+	}
 }
+
+func createErrorResponse(err error) map[string]interface{} {
+	var errorResponse map[string]interface{}
+	var syntaxError *json.SyntaxError
+	var unmarshalError *json.UnmarshalTypeError
+	switch {
+	case errors.As(err, &syntaxError):
+		errorResponse = map[string]interface{}{
+			"error": fmt.Sprintf("Request contains badly-formed JSON (at position %d)", syntaxError.Offset),
+			"code":  "bad_request",
+		}
+	
+	case errors.Is(err, io.ErrUnexpectedEOF):
+		errorResponse = map[string]interface{}{
+			"error": "Request contains badly-formed JSON",
+			"code":  "bad_request",
+		}
+	
+	case errors.As(err, &unmarshalError):
+		errorResponse = map[string]interface{}{
+			"error": fmt.Sprintf("Request contains an invalid value for the %q field (at position %d)", 
+				unmarshalError.Field, unmarshalError.Offset),
+			"code":  "bad_request",
+		}
+	
+	case strings.HasPrefix(err.Error(), "json: unknown field "):
+		fieldName := strings.TrimPrefix(err.Error(), "json: unknown field ")
+		errorResponse = map[string]interface{}{
+			"error": fmt.Sprintf("Request contains unknown field: %s", fieldName),
+			"code":  "bad_request",
+		}
+	
+	case errors.Is(err, io.EOF):
+		errorResponse = map[string]interface{}{
+			"error": "Request must not be empty",
+			"code":  "bad_request",
+		}
+	
+	default:
+		errorResponse = map[string]interface{}{
+			"error": "Internal server error",
+			"code":  "server_error",
+		}
+		log.Printf("Unhandled error: %v", err)
+	}
+	
+	return errorResponse
+	}
 
